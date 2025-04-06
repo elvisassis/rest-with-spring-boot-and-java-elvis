d40f8aecb4dc153795a6a481120835d91469f253
Elvis Assis <kesleiassis@gmail.com>
2025-04-06 14:46:41 -0300

    Criação de um teste simples de integração para a entidade Person que será refatorado criando uma classe especifica para a entide e a corbertura de todos metodos da PersonController

--- 09_WorkingWithDTOPattern/src/test/java/br/com/elvisassis/integrationtests/swagger/SwaggerIntegrationTest.java
+++ 09_WorkingWithDTOPattern/src/test/java/br/com/elvisassis/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -1,13 +1,15 @@
 package br.com.elvisassis.integrationtests.swagger;
 
 import br.com.elvisassis.config.TestConfigs;
+import br.com.elvisassis.data.dto.PersonDTO;
 import br.com.elvisassis.integrationtests.testconstainers.AbstractIntegrationTest;
+import br.com.elvisassis.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SwaggerIntegrationTest extends AbstractIntegrationTest {
@@ -30,5 +32,25 @@ class SwaggerIntegrationTest extends AbstractIntegrationTest {
 
         assertTrue(content.contains("Swagger UI"));
     }
+
+    //TODO criar uma classe especfica para testar a entidade Person
+    @Test
+    void TestPeopleFindByIdWithReturningStatus200Ok(){
+
+       PersonDTO personDTO = given()
+            .basePath("/api/person/v1/2")
+                .port(port)
+            .when()
+                .get()
+            .then()
+                .statusCode(200)
+            .extract()
+               .as(PersonDTO.class);
+
+       assertNotNull(personDTO);
+       assertEquals("Bia", personDTO.getFirstName());
+       assertEquals("Verde Reis", personDTO.getLastName());
+    }
+
 
 }
