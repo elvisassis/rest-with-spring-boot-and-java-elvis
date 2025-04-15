f4941f8f735af878e0d7258ca0408fd43a0a9f99
Elvis Assis <kesleiassis@gmail.com>
2025-04-15 16:36:20 -0300

    Crição da estrura inicial para exportação de xlsx e csv

---
+++ 09_WorkingWithDTOPattern/src/main/java/br/com/elvisassis/file/exporter/contract/FileExporter.java
@@ -0,0 +1,1 @@
+package br.com.elvisassis.file.exporter.contract;import br.com.elvisassis.data.dto.PersonDTO;import org.springframework.core.io.Resource;import java.io.InputStream;import java.util.List;public interface FileExporter {    Resource exportFile(List<PersonDTO> people) throws Exception;}
