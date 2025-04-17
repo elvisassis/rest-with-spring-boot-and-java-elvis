9295de5e53a0bd79e18b7ae4b6f22e53bb71afac
Elvis Assis <kesleiassis@gmail.com>
2025-04-17 10:59:26 -0300

    Implementação da geração de relatório em pdf usando o jasper report e a inclusão de migrations para associar person a book e book a person

--- 09_WorkingWithDTOPattern/src/main/java/br/com/elvisassis/file/exporter/MediaTypes.java
+++ 09_WorkingWithDTOPattern/src/main/java/br/com/elvisassis/file/exporter/MediaTypes.java
@@ -1,1 +1,1 @@
-package br.com.elvisassis.file.exporter;public interface MediaTypes {    String APPLICATION_XLSX_VALUE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";    String APPLICATION_CSV_VALUE = "text/csv";}
+package br.com.elvisassis.file.exporter;public interface MediaTypes {    String APPLICATION_XLSX_VALUE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";    String APPLICATION_CSV_VALUE = "text/csv";    String APPLICATION_PDF_VALUE = "application/pdf";}
